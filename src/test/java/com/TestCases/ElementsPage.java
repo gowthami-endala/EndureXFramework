eb0ddbef3059f4de3dde0294665c8003221af24f
gowthami <gowthami@192.168.0.230>
2024-02-21 15:22:31 -0800

    Added Retry Analyzer

--- src/test/java/com/TestCases/ElementsPage.java
+++ src/test/java/com/TestCases/ElementsPage.java
@@ -8,24 +8,32 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 
 import com.driver.DriverManager;
+import com.driver.RetryAnalyzer;
 
 public class ElementsPage extends BasePage {
 	@SuppressWarnings("deprecation")
 	@Test(groups = { "Regression", "ElementsPage" })
+	
+	
+	
 public void ElementsPage() {
 		DriverManager.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		DriverManager.getDriver().findElement(By.xpath("//img[@src='/images/Toolsqa.jpg']")).click();
 		DriverManager.getDriver().get("https://demoqa.com/elements");
 		DriverManager.getDriver().findElement(By.xpath("//div[@class='header-text'][normalize-space()='Elements']")).click();
 	}
-	@Test(groups = { "Regression", "ElementsPage" })
+    @Test(retryAnalyzer = RetryAnalyzer.class)
 	public void TextBoxField() {
+		
 	DriverManager.getDriver().get("https://demoqa.com/text-box");
+	 //fail test scenario
+		//DriverManager.getDriver().findElement(By.id("permanentAddress")).sendKeys("1621 S Wolfe Road, CA");
 	DriverManager.getDriver().findElement(By.id("userName")).sendKeys("Test1");
 	DriverManager.getDriver().findElement(By.id("userEmail")).sendKeys("Test1@gmail.com");
 	DriverManager.getDriver().findElement(By.id("currentAddress")).sendKeys("1621 S Wolfe Road, CA");
 	DriverManager.getDriver().findElement(By.id("permanentAddress")).sendKeys("1621 S Wolfe Road, CA");
-    DriverManager.getDriver().findElement(By.id("submit")).click();
+    //DriverManager.getDriver().findElement(By.id("submit")).click();
+   
 	}
 	@Test(groups = { "Regression", "ElementsPage" })
 	public void CheckBoxField() {
--- src/test/java/com/TestCases/FormsPage.java
+++ src/test/java/com/TestCases/FormsPage.java
@@ -1,18 +1,31 @@
 package com.TestCases;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+import org.assertj.core.api.Assertions;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-import com.driver.*;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.assertj.core.api.Assertions;
+import com.driver.DriverManager;
+import com.driver.RetryAnalyzer;
+
 
 public class FormsPage extends BasePage{
 	
-	@Test
+    @Test(retryAnalyzer = RetryAnalyzer.class)
+
 	public void FormsPage() {
 		DriverManager.getDriver().get("https://demoqa.com/automation-practice-form");
 		DriverManager.getDriver().findElement(By.xpath("//div[@class='element-list collapse show']//li[@id='item-0']"));
 		Assert.assertEquals("Student Registration Form",DriverManager.getDriver().findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/div[2]/div[2]/h5[1]")).getText()) ;
+		String title = DriverManager.getDriver().getTitle();
+		String ExpectedTitle = "DEMOQA";
+		Assert.assertEquals(title,"DEMOQA");
 
+	
 	}
 
 }
