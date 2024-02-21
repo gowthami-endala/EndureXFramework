ae4b7d23a1d1298697ce170e5d71a941aae657f6
gowthami <gowthami@192.168.0.230>
2024-02-21 15:22:51 -0800

    Added Retry Analyzer

---
+++ src/main/java/com/driver/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package com.driver;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+    private static final int MAX_RETRY_COUNT = 3; // Maximum number of retries
+
+    private int retryCount = 0;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < MAX_RETRY_COUNT) {
+            retryCount++;
+            return true; // Retry the test
+        }
+        return false; // Do not retry the test
+    }
+}
